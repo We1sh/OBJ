9d131dd05ac315685e634e977bd21e3876911170
We1sh <dimajio1106@gmail.com>
2021-09-18 18:56:42 +0300

    zz1

---
+++ Zz1.py
@@ -0,0 +1,12 @@
+class Football:
+    def name(self):
+        print('Дзюба','Маркевич','Рональдо','Месси')
+    def pos(self):
+        print('1','45','56','37')
+        
+def main():
+    foot=Football()
+    foot.name()
+    foot.pos()
+main()
+#Вроде сделал
